9b4cdb7955dac68e04a9a689556c15f84ff291ca
Kazushige Okuma <kazokuma@gmail.com>
2021-06-05 15:55:50 -0400

    Finished Gatsby Tutorial Part 2

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,17 @@
-// Step 1: Import React
+// Step 1: Import your component
 import * as React from 'react';
 import Layout from "../components/layout";
 
 // Step 2: Define your component
-const IndexPage = () => {
+const AboutPage = () => {
   return (
     <main>
-      <Layout pageTitle="Home Page">
-        <p>I'm making this at Home by following the Gatsby Tutorial.</p>
+      <Layout pageTitle="About Me">
+        <p>I'm making this at About by following the Gatsby Tutorial.</p>
       </Layout>
     </main>
   )
 }
 
 // Step 3: Export your component
-export default IndexPage
+export default AboutPage
